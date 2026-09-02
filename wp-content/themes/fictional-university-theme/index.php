76889898921e383c7a090df77f3e03f05e18f28d
Nihad <nihad.nemetli@gmail.com>
2026-09-02 19:48:22 +0400

    add: famous loop created

--- wp-content/themes/fictional-university-theme/index.php
+++ wp-content/themes/fictional-university-theme/index.php
@@ -1,2 +1,10 @@
-<h1><?php bloginfo('name') ?></h1>
-<p><?php bloginfo('description') ?></p>
+<?php
+    while (have_posts()) {
+        the_post();
+     
+?>
+    <h2><?php the_title(); ?></h2>
+    <p><?php the_content(); ?></p>
+<?php
+    }
+?>
