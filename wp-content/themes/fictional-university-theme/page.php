9476e053ff9e706285d692fd0769605820bc24fd
Nihad <nihad.nemetli@gmail.com>
2026-09-02 19:56:35 +0400

    upd: page updated

--- wp-content/themes/fictional-university-theme/page.php
+++ wp-content/themes/fictional-university-theme/page.php
@@ -1,1 +1,11 @@
-This is test oage
+<?php
+    while (have_posts()) {
+        the_post();
+     
+?>
+    <h1>This is a page not a post</h2>
+    <h2><?php the_title(); ?></h2>
+    <p><?php the_content(); ?></p>
+<?php
+    }
+?>
