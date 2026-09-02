85888c5cc1e83f9ba2d7f23078dced94751497d3
Nihad <nihad.nemetli@gmail.com>
2026-09-02 19:49:45 +0400

    add: link added

--- wp-content/themes/fictional-university-theme/index.php
+++ wp-content/themes/fictional-university-theme/index.php
@@ -3,8 +3,9 @@
         the_post();
      
 ?>
-    <h2><?php the_title(); ?></h2>
+    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
     <p><?php the_content(); ?></p>
+    <hr>
 <?php
     }
 ?>
