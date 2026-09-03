00a6d52a29935649979e46a584201e74aa7c487b
Nihad <nihad.nemetli@gmail.com>
2026-09-04 03:04:28 +0400

    upd: header updated

--- wp-content/themes/fictional-university-theme/header.php
+++ wp-content/themes/fictional-university-theme/header.php
@@ -1,1 +1,12 @@
-<h1>Greetings from header.php</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?>
+    <title>Fictional University</title>
+</head>
+<body>
+    <h1>Fictional University</h1>
+</body>
+</html>
