9c53582e0220c84e8f5c8caf9d2f09c5a34a705c
Nihad <nihad.nemetli@gmail.com>
2026-09-04 02:54:00 +0400

    upd: index writed more clean way

--- wp-content/themes/fictional-university-theme/index.php
+++ wp-content/themes/fictional-university-theme/index.php
@@ -1,11 +1,11 @@
-<?php
-    while (have_posts()) {
-        the_post();
-     
-?>
+<h1>This is header area</h1>
+
+<?php while (have_posts()): the_post(); ?>
+    
     <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
     <p><?php the_content(); ?></p>
     <hr>
-<?php
-    }
-?>
+
+<?php endwhile ?>
+
+<h1>This is footer area</h1>
