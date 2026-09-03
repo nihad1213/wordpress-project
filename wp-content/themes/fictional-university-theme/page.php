dee834869146ee44587c99a7ae26173d0d17ab0b
Nihad <nihad.nemetli@gmail.com>
2026-09-04 03:00:31 +0400

    add: footer and header added to page file

--- wp-content/themes/fictional-university-theme/page.php
+++ wp-content/themes/fictional-university-theme/page.php
@@ -1,11 +1,9 @@
-<?php
-    while (have_posts()) {
-        the_post();
-     
-?>
-    <h1>This is a page not a post</h2>
+<?php get_header(); ?>
+
+<?php while (have_posts()): the_post(); ?>
+    <h1>This is a page not a post</h1>
     <h2><?php the_title(); ?></h2>
     <p><?php the_content(); ?></p>
-<?php
-    }
-?>
+<?php endwhile; ?>
+
+<?php get_footer(); ?>
