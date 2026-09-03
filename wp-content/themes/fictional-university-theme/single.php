78c6ca6991ab4f985efd1fb3e0f6a6629b9b2b91
Nihad <nihad.nemetli@gmail.com>
2026-09-04 02:59:12 +0400

    add: header and footer added to single file

--- wp-content/themes/fictional-university-theme/single.php
+++ wp-content/themes/fictional-university-theme/single.php
@@ -1,10 +1,8 @@
-<?php
-    while (have_posts()) {
-        the_post();
-     
-?>
-    <h2><?php the_title(); ?></h2>
-    <p><?php the_content(); ?></p>
-<?php
-    }
-?>
+<?php get_header(); ?>
+
+<?php while (have_posts()): the_post(); ?>
+    <h2><?= the_title(); ?></h2>
+    <p><?= the_content(); ?></p>
+<?php endwhile; ?>
+
+<?php get_footer(); ?>
