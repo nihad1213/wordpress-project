7dc33150018b6fc914255db37a1058a60ea28cc0
Nihad <nihad.nemetli@gmail.com>
2026-09-07 23:02:49 +0400

    upd: page part writed as php 8

--- wp-content/themes/fictional-university-theme/page.php
+++ wp-content/themes/fictional-university-theme/page.php
@@ -1,12 +1,9 @@
-<?php
+<?php get_header(); ?>
 
-  get_header();
-
-  while(have_posts()) {
-    the_post(); ?>
+<?php while (have_posts()) : the_post(); ?>
     
-    <div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
+  <div class="page-banner">
+    <div class="page-banner__bg-image" style="background-image: url(<?= esc_url(get_theme_file_uri('/images/ocean.jpg')) ?>);"></div>
     <div class="page-banner__content container container--narrow">
       <h1 class="page-banner__title"><?php the_title(); ?></h1>
       <div class="page-banner__intro">
@@ -18,7 +15,7 @@
   <div class="container container--narrow page-section">
 
     <div class="metabox metabox--position-up metabox--with-home-link">
-      <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span></p>
+      <p><a class="metabox__blog-home-link" href="<?= esc_url(site_url('/about-us')) ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main"><?php the_title(); ?></span></p>
     </div>
     
     <!--
@@ -36,9 +33,7 @@
     </div>
 
   </div>
-    
-  <?php }
 
-  get_footer();
+<?php endwhile; ?>
 
-?>
+<?php get_footer(); ?>
