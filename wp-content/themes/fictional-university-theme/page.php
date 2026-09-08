6b9a0c5f33e0d0e3fe037f0b25079587a42009f8
Nihad <nihad.nemetli@gmail.com>
2026-09-08 21:08:46 +0400

    upd: page.php if statement added for parent and cd child page

--- wp-content/themes/fictional-university-theme/page.php
+++ wp-content/themes/fictional-university-theme/page.php
@@ -13,21 +13,17 @@
   </div>
 
   <div class="container container--narrow page-section">
-
+  
+  <?php if (wp_get_post_parent_id()): ?>
     <div class="metabox metabox--position-up metabox--with-home-link">
-      <p><a class="metabox__blog-home-link" href="<?= esc_url(site_url('/about-us')) ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main"><?php the_title(); ?></span></p>
+      <p>
+        <a class="metabox__blog-home-link" href="<?= esc_url(site_url('/about-us')) ?>">
+        <i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> 
+        <span class="metabox__main"><?php the_title(); ?></span>
+      </p>
     </div>
-    
-    <!--
-    <div class="page-links">
-      <h2 class="page-links__title"><a href="#">About Us</a></h2>
-      <ul class="min-list">
-        <li class="current_page_item"><a href="#">Our History</a></li>
-        <li><a href="#">Our Goals</a></li>
-      </ul>
-    </div>
-    -->
-
+  <?php endif; ?>
+  
     <div class="generic-content">
       <?php the_content(); ?>
     </div>
