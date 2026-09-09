da018f87b265c762a9a4b71cd5ff329ffea3243e
Nihad <nihad.nemetli@gmail.com>
2026-09-09 22:24:23 +0400

    rem: header bar removed

--- wp-content/themes/fictional-university-theme/header.php
+++ wp-content/themes/fictional-university-theme/header.php
@@ -12,7 +12,7 @@
           <a href="<?= esc_url(site_url('/')) ?>"><strong>Fictional</strong> University</a>
         </h1>
         <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
-        <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
+              <!-- <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i> -->
         <div class="site-header__menu group">
           <nav class="main-navigation">
             <ul>
@@ -26,7 +26,9 @@
           <div class="site-header__util">
             <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
             <a href="#" class="btn btn--small btn--dark-orange float-left">Sign Up</a>
-            <span class="search-trigger js-search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
+            <span class="search-trigger js-search-trigger">
+              <i class="fa fa-search" aria-hidden="true"></i>
+            </span>
           </div>
         </div>
       </div>
