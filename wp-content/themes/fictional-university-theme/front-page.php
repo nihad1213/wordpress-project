61741a47edcfafb3a77bf65ec01a78b18ab27870
Nihad <nihad.nemetli@gmail.com>
2026-09-14 17:53:33 +0400

    add: backlshas added for hide warnings

--- wp-content/themes/fictional-university-theme/front-page.php
+++ wp-content/themes/fictional-university-theme/front-page.php
@@ -16,7 +16,7 @@
           <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
 
           <?php
-          $homepage_events = new WP_Query(array(
+          $homepage_events = new \WP_Query(array(
             'posts_per_page' => 2,
             'post_type'      => 'event'
           ));
@@ -58,7 +58,7 @@
           <h2 class="headline headline--small-plus t-center">From Our Blogs</h2>
 
           <?php
-          $blog_posts = new WP_Query(array(
+          $blog_posts = new \WP_Query(array(
             'posts_per_page' => 2
           ));
 
