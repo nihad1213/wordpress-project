601cc208e76a5797c84f43708eb9f223b6c7c6c1
Nihad <nihad.nemetli@gmail.com>
2026-09-14 17:49:32 +0400

    upd: blog posts trim logic added

--- wp-content/themes/fictional-university-theme/front-page.php
+++ wp-content/themes/fictional-university-theme/front-page.php
@@ -69,9 +69,19 @@
                 <span class="event-summary__day"><?php the_time('d'); ?></span>
               </a>
               <div class="event-summary__content">
-                <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-                <?php the_excerpt(); ?>
-                <p class="no-margin"><a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
+                <h5 class="event-summary__title headline headline--tiny">
+                  <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                </h5>
+                <p>
+                  <?php 
+                  if (has_excerpt()) {
+                    echo get_the_excerpt();
+                  } else {
+                    echo wp_trim_words(get_the_content(), 18);
+                  } 
+                  ?> 
+                  <a href="<?php the_permalink(); ?>" class="nu gray">Read more</a>
+                </p>
               </div>
             </div>
           <?php endwhile;
