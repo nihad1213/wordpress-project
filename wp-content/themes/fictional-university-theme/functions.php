1d2e136fa9d5f16ab620acc71aaf754b853bb22a
Nihad <nihad.nemetli@gmail.com>
2026-09-17 13:29:26 +0400

    upd: new files added

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -1,64 +1,46 @@
 <?php
 
 function university_files() {
-    wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
-    wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
-    wp_enqueue_style('font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
-    wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
-    wp_enqueue_script('main-university-js', get_theme_file_uri('/build/index.js'), 
-        array('jquery'), '1.0', true);
+  wp_enqueue_script('main-university-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
+  wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+  wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+  wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
+  wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
 }
 
 add_action('wp_enqueue_scripts', 'university_files');
 
 function university_features() {
-    add_theme_support('title-tag');
+  add_theme_support('title-tag');
+  add_theme_support('post-thumbnails');
+  add_image_size('professorLandscape', 400, 260, true);
+  add_image_size('professorPortrait', 480, 650, true);
+  add_image_size('pageBanner', 1500, 350, true);
 }
 
 add_action('after_setup_theme', 'university_features');
 
+function university_adjust_queries($query) {
+  if (!is_admin() AND is_post_type_archive('program') AND $query->is_main_query()) {
+    $query->set('orderby', 'title');
+    $query->set('order', 'ASC');
+    $query->set('posts_per_page', -1);
+  }
 
-
-// Add Meta Box to Event Post Type
-function add_event_program_metabox() {
-    add_meta_box(
-        'event_related_program',
-        'Related Program',
-        'render_event_program_metabox',
-        'event',
-        'side',
-        'default'
-    );
-}
-add_action('add_meta_boxes', 'add_event_program_metabox');
-
-function render_event_program_metabox($post) {
-    wp_nonce_field('save_event_program', 'event_program_nonce');
-    $selected_program = get_post_meta($post->ID, '_related_program_id', true);
-    
-    $programs = get_posts([
-        'post_type' => 'program',
-        'numberposts' => -1,
-        'orderby' => 'title',
-        'order' => 'ASC'
-    ]);
-
-    echo '<select name="related_program_id" style="width:100%;">';
-    echo '<option value="">-- Select Program --</option>';
-    foreach ($programs as $program) {
-        $selected = ($selected_program == $program->ID) ? 'selected' : '';
-        echo '<option value="' . esc_attr($program->ID) . '" ' . $selected . '>' . esc_html($program->post_title) . '</option>';
-    }
-    echo '</select>';
+  if (!is_admin() AND is_post_type_archive('event') AND $query->is_main_query()) {
+    $today = date('Ymd');
+    $query->set('meta_key', 'event_date');
+    $query->set('orderby', 'meta_value_num');
+    $query->set('order', 'ASC');
+    $query->set('meta_query', array(
+              array(
+                'key' => 'event_date',
+                'compare' => '>=',
+                'value' => $today,
+                'type' => 'numeric'
+              )
+            ));
+  }
 }
 
-function save_event_program_metabox($post_id) {
-    if (!isset($_POST['event_program_nonce']) || !wp_verify_nonce($_POST['event_program_nonce'], 'save_event_program')) return;
-    if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) return;
-    if (!current_user_can('edit_post', $post_id)) return;
-
-    if (isset($_POST['related_program_id'])) {
-        update_post_meta($post_id, '_related_program_id', sanitize_text_field($_POST['related_program_id']));
-    }
-}
-add_action('save_post', 'save_event_program_metabox');
+add_action('pre_get_posts', 'university_adjust_queries');
--- wp-content/themes/fictional-university-theme/index.php
+++ wp-content/themes/fictional-university-theme/index.php
@@ -1,34 +1,38 @@
-<?php get_header() ?>
-  
-  <div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(<?= esc_url(get_theme_file_uri('/images/ocean.jpg')) ?>);"></div>
-    <div class="page-banner__content container container--narrow">
-      <h1 class="page-banner__title">Welcome to Our Blog!</h1>
-      <div class="page-banner__intro">
-        <p>Keep up with the latest news and updates from our university.</p>
-      </div>
-    </div>  
-  </div>
+<?php get_header(); ?>
 
-  <div class="container container--narrow page-section">
-  <?php while(have_posts()): the_post(); ?>
+<div class="page-banner">
+  <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg'); ?>);"></div>
+  <div class="page-banner__content container container--narrow">
+    <h1 class="page-banner__title">Welcome to our blog!</h1>
+    <div class="page-banner__intro">
+      <p>Keep up with our latest news.</p>
+    </div>
+  </div>  
+</div>
+
+<div class="container container--narrow page-section">
+<?php
+  while (have_posts()) : 
+    the_post(); 
+?>
     <div class="post-item">
-      <h2><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
+      <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
       
       <div class="metabox">
-        <p>Posted by <?php the_author() ?> on <?php the_time('F j, Y') ?> in <?= get_the_category_list(', ') ?></p>
+        <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time('n.j.y'); ?> in <?php echo get_the_category_list(', '); ?></p>
       </div>
 
       <div class="generic-content">
-        <?php the_excerpt() ?>
-        <p>
-          <a class="btn btn--blue" href="<?php the_permalink() ?>">Continue Reading &raquo;</a>
-        </p>
+        <?php the_excerpt(); ?>
+        <p><a class="btn btn--blue" href="<?php the_permalink(); ?>">Continue reading &raquo;</a></p>
       </div>
-    </div>
-  <?php endwhile;
-  echo paginate_links();
-  ?>
-  </div>
 
-<?php get_footer()  ?>
+    </div>
+<?php 
+  endwhile; 
+  
+  echo paginate_links();
+?>
+</div>
+
+<?php get_footer(); ?>
